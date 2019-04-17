916b94ffd1cc69f2fbb7bd405c8e51f723f7b69b
Vincent Allen <vtadf@mst.edu>
2019-04-17 03:40:13 +0100

    Updating helper file for the python files converted from m files

--- src-main/slicing.py
+++ src-main/slicing.py
@@ -1,16 +1,23 @@
 
 
+def SetRange(array2d, in2d, new_value)
+   """
+   """
+   
+   
+   
 def SetRange(array2d, row_str, col_str, new_values)
    """
       @brief  Set the specified rows and cols in the array,
               where the array2d is something like: [ [ x, y, z ],
                                                      [ a, b, c ] ]
    """
-   for row_i in range(*InterpretSliceStr(row_str, GetNumRows(row_str))):
-      for col_i in range(*InterpretSliceStr(col_str, GetNumCols(col_str))):
-         array2d[row_i][col_i] = new_values[row_i][col_i]
-         
-         
+   if Is2d(array2d):
+      for row_i in range(*InterpretSliceStr(row_str, GetNumRows(row_str))):
+         for col_i in range(*InterpretSliceStr(col_str, GetNumCols(col_str))):
+            array2d[row_i][col_i] = new_values[row_i][col_i]
+            
+            
 def GetRange(array2d, row_str, col_str):
    """
       @brief  Get the specified rows and cols from the array,
@@ -18,12 +25,19 @@ def GetRange(array2d, row_str, col_str):
                                                      [ a, b, c ] ]
    """
    new_array = []
-   for row_i in range(*InterpretSliceStr(row_str, GetNumRows(row_str))):
-      new_row = []
-      for col_i in range(*InterpretSliceStr(col_str, GetNumCols(col_str))):
-         new_row.append(array2d[row_i][col_i])
-      new_array.append(new_row)
+   if Is2d(array2d):
+      for row_i in range(*InterpretSliceStr(row_str, GetNumRows(row_str))):
+         new_row = []
+         for col_i in range(*InterpretSliceStr(col_str, GetNumCols(col_str))):
+            new_row.append(array2d[row_i][col_i])
+         new_array.append(new_row)
    return new_array
+   
+   
+def Is2d(array2d):
+   """
+   """
+   return array2d and array2d[0]
    
    
 def GetNumRows(array2d):
