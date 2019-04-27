150c4209b1b1c5c91ce13000adab74621ff94dff
Vincent Allen <vtadf@mst.edu>
2019-04-27 04:13:26 +0100

    Using the same untested sort I tried with smop

--- src-main/my_src.py
+++ src-main/my_src.py
@@ -51,6 +51,27 @@ def my_pause():
    # Input() # TODO - uncomment in final revision
    
    
+def my_sort(numpy_ndarray, n, descr): # TODO - does n matter?
+   """
+      https://stackoverflow.com/questions/28512237/python-equivalent-to-matlab-a-b-sorty
+      https://stackoverflow.com/questions/26984414/efficiently-sorting-a-numpy-array-in-descending-order
+      
+      Using mergesort b/c it is clean
+      
+      TODO - haven't tested this method!
+   """
+   # print(numpy_ndarray)
+   cpy = numpy_ndarray.copy()
+   if descr == 'ascend':
+      cpy.sort(kind='mergesort') # TODO - need to specify axis?
+   elif descr == 'descend':
+      cpy[::-1].sort(kind='mergesort') # TODO - argsort if descending?
+   else:
+      raise ValueError(descr)
+   # print(cpy)
+   return cpy, None # TODO - numpy argsort
+   
+   
 ################################################################################
 #
 #  SMOP only
@@ -63,25 +84,6 @@ def my_disp(*args):
    """
    print(' '.join(args))
    sys.stdout.flush()
-   
-   
-def my_sort(numpy_ndarray, x, descr): # TODO - does x matter?
-   """
-      https://stackoverflow.com/questions/28512237/python-equivalent-to-matlab-a-b-sorty
-      https://stackoverflow.com/questions/26984414/efficiently-sorting-a-numpy-array-in-descending-order
-      
-      Using mergesort b/c it is clean
-   """
-   # print(numpy_ndarray)
-   cpy = numpy_ndarray.copy()
-   if descr == 'ascend':
-      cpy.sort(kind='mergesort') # TODO - need to specify axis?
-   elif descr == 'descend':
-      cpy[::-1].sort(kind='mergesort') # TODO - argsort if descending?
-   else:
-      raise ValueError(descr)
-   # print(cpy)
-   return cpy, None
    
    
 def my_norm(*args):
